4dbc8f4f0f4b1b44c4eb66fbd7a694621a219b1d
Leves <levente@bbco.hu>
2025-04-13 13:26:17 +0200

    implemented repository

--- BACKEND/Data/TeamClusterRepository.cs
+++ BACKEND/Data/TeamClusterRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace BACKEND.Data
+﻿using BACKEND.Models;
+
+namespace BACKEND.Data
 {
-    public class TeamClusterRepository
+    public class TeamClusterRepository : ITeamClusterRepository
     {
+        private List<TeamCluster> _data;
+
+        private int _id;
+        private int GetNextId() => _id++;
+
+        public TeamClusterRepository()
+        {
+            _data = new List<TeamCluster>();
+        }
+
+        public void Create(TeamCluster teamCluster)
+        {
+            teamCluster.Id = GetNextId();
+            _data.Add(teamCluster);
+        }
+
+        public void Delete(int id)
+        {
+            _data.Remove(_data.FirstOrDefault(x => x.Id == id));
+        }
+
+        public IEnumerable<TeamCluster> Read()
+        {
+            return _data;
+        }
+
+        public TeamCluster? Read(int id)
+        {
+            return _data.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(TeamCluster teamCluster)
+        {
+            TeamCluster teamClusterToUpdate = _data.FirstOrDefault(x => x.Id == teamCluster.Id);
+            teamClusterToUpdate.Teams = teamCluster.Teams;
+        }
     }
 }
