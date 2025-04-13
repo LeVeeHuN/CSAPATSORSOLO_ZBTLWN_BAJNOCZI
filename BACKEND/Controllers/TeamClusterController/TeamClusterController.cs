b689e7e27b16d3647fdcc8f707407e3fbbbeb9dd
Leves <levente@bbco.hu>
2025-04-13 13:41:52 +0200

    Implemented Delete on controller

--- BACKEND/Controllers/TeamClusterController/TeamClusterController.cs
+++ BACKEND/Controllers/TeamClusterController/TeamClusterController.cs
@@ -57,7 +57,12 @@ namespace BACKEND.Controllers.TeamClusterController
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            // Check if ID is valid
+            TeamCluster? teamCluster = _repo.Read(id);
+            if (teamCluster != null)
+            {
+                _repo.Delete(id);
+            }
         }
 
         #endregion
