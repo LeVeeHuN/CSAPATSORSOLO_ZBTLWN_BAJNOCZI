3e39c34f696697cff217fa5f3e62825cdf127b57
Leves <levente@bbco.hu>
2025-04-13 13:20:17 +0200

    Added repository

---
+++ BACKEND/Data/ITeamClusterRepository.cs
@@ -0,0 +1,24 @@
+﻿using BACKEND.Models;
+
+namespace BACKEND.Data
+{
+    public interface ITeamClusterRepository
+    {
+        // CRUD
+        /*
+         *  - Create
+         *  - Read
+         *  - Update
+         *  - Delete
+         */
+
+        void Create(TeamCluster teamCluster);
+
+        IEnumerable<TeamCluster> Read();
+        TeamCluster Read(int id);
+
+        void Update(TeamCluster teamCluster);
+
+        void Delete(int id);
+    }
+}
